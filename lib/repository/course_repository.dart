47a889146f48bd964b14329f154d5e7005ef2e8d
Adnan Turgay Aydin <taydinadnan@gmail.com>
2023-09-25 12:19:55 +0200

    Use a Model & repository

---
+++ lib/repository/course_repository.dart
@@ -0,0 +1,20 @@
+import 'package:rwcourses/constants.dart';
+import 'package:rwcourses/model/course.dart';
+import 'package:rwcourses/repository/repository.dart';
+
+class CourseRepository implements Repository {
+  String dataURL =
+      'https://api.raywenderlich.com/api/contents?filter[content_types][]=collection';
+
+  @override
+  Future<List<Course>> getCourses(int domainFilter) async {
+    final courses = <Course>[];
+    var url = dataURL;
+
+    if (domainFilter != Constants.allFilter) {
+      url += ';&filter[domain_ids][]=$domainFilter';
+    }
+
+    return courses;
+  }
+}
---
+++ lib/repository/repository.dart
@@ -0,0 +1,5 @@
+import 'package:rwcourses/model/course.dart';
+
+abstract class Repository {
+  Future<List<Course>> getCourses(int domainFilter);
+}
